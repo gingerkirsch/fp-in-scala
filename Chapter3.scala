212d11dfe34d8dac2f0b51bf360ce44b71742ace
Sasha Sankova <sandra.sankova@gmail.com>
2018-01-07 11:05:59 +0000

    Maps, filters, flatten, zips

--- Chapter3.scala
+++ Chapter3.scala
@@ -87,5 +87,33 @@ object List {
   def sumLeft(ns: List[Int]) = foldLeft(ns, 0)(_ + _)
   def productLeft(ns: List[Double]) = foldLeft(ns, 1.0)(_ * _)
   def lengthLeft[A](as: List[A]): Int = foldLeft(as, 0)((n, _) => n + 1)
+  def reverse[A](as: List[A]) = foldLeft(as, Nil: List[A])((x,y) => Cons(y,x))
 
+  def foldLeftViaFoldRight[A,B](as: List[A], z: B)(f: (B,A) => B): B =
+    foldRight(as, (b: B) => b)((a, g) => b => g(f(b, a)))(z)
+
+  def foldRightViaFoldLeft[A,B](as: List[A], z: B)(f: (A,B) => B): B =
+    foldLeft(reverse(as),z)((x,y) => f(y,x))
+
+  def appendViaFold[A](a1: List[A], a2: List[A]): List[A] = foldRight(a1,a2)(Cons(_,_))
+  def flatten[A](l: List[List[A]]): List[A] = foldRight(l, Nil:List[A])(appendViaFold(_,_))
+  def add1(l: List[Int]): List[Int] = foldRight(l, Nil: List[Int])((a,b) => Cons(a+1,b))
+  def toString(l: List[Double]): List[String] = foldRight(l, Nil: List[String])((a,b) => Cons(a.toString, b))
+  def map[A,B](l: List[A])(f: A => B): List[B] = foldRight(l, Nil: List[B])((a,b) => Cons(f(a),b))
+  def filter[A](l: List[A])(f: A => Boolean): List[A] =
+    foldRight(l, Nil: List[A])((a,b) => if (f(a)) Cons(a,b) else b)
+  def flatMap[A,B](l: List[A])(f: A => List[B]): List[B] = foldRight(l, Nil: List[B])((a,b) => appendViaFold(f(a),b))
+  def filterViaFlatMap[A](l: List[A])(f: A => Boolean): List[A] = flatMap(l)(a => if (f(a)) List(a) else Nil)
+
+  def sumLists(a1: List[Int], a2: List[Int]): List[Int] = (a1,a2) match {
+    case (Cons(x, xs), Cons(y, ys)) => Cons(x + y, sumLists(xs, ys))
+    case _ => Nil
+  }
+
+  def zipWith[A](a1: List[A], a2: List[A])(f: (A,A) => A): List[A] = (a1,a2) match {
+    case (Cons(x, xs), Cons(y, ys)) => Cons(f(x,y), zipWith(xs,ys)(f))
+    case _ => Nil
+  }
+
+  def hasSubsequence[A](sup: List[A], sub: List[A]): Boolean = ???
 }
