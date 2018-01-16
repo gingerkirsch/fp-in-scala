dcd0fcacb27b8a1ba53478fed7f23c03e89032af
Sasha Sankova <sandra.sankova@gmail.com>
2018-01-16 00:09:38 +0000

    Add map2, sequence, traverse

--- fpinscala/chapter4/Option.scala
+++ fpinscala/chapter4/Option.scala
@@ -30,6 +30,28 @@ sealed trait Option1[+A] {
 
   def variance(xs: Seq[Double]): Option1[Double] =
     mean(xs).flatMap(m => mean(xs.map(x => math.pow(x - m,2))))
+
+  def map2[A,B,C](a: Option1[A], b: Option1[B])(f: (A,B) => C): Option1[C] =
+    (a,b) match {
+      case (Some1(a), Some1(b)) => Some1(f(a,b))
+      case (_,_) => None1
+    }
+
+  def sequence[A](a: List[Option1[A]]): Option1[List[A]] =
+    a.foldRight[Option1[List[A]]](Some1(Nil))((a,b) => map2(a,b)(_ :: _))
+
+  def traverse[A,B](a: List[A])(f: A => Option1[B]): Option1[List[B]] =
+    a.foldRight[Option1[List[B]]](Some1(Nil))((a,b) => map2(f(a),b)(_ :: _))
+
+  def map2_1[A,B,C](a: Option1[A], b: Option1[B])(f: (A,B) => C): Option1[C] =
+    a flatMap (aa => b map (bb => f(aa,bb)))
+
+  def map2_2[A,B,C](a: Option1[A], b: Option1[B])(f: (A,B) => C): Option1[C] =
+    for {
+      aa <- a
+      bb <- b
+    } yield f(aa, bb)
+
 }
 
 case class Some1[+A](get: A) extends Option1[A]
