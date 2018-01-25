6b8db0db6b2750c8036381bf2938e02f3df2256c
Aliaksandra Sankova <aliaksandra.sankova@enear.co>
2018-01-25 00:25:13 +0000

    Exercises until unfold

--- fpinscala/chapter5/Stream.scala
+++ fpinscala/chapter5/Stream.scala
@@ -41,6 +41,40 @@ sealed trait Stream[+A] {
     case Cons(h,t) if (p(h())) => cons(h(), takeWhile(p))
     case _ => empty
   }
+
+  def exists(p: A => Boolean): Boolean = this match {
+    case Cons(h, t) => p(h()) || t().exists(p)
+    case _ => false
+  }
+
+  def foldRight[B](z: => B)(f: (A, => B) => B): B = this match {
+    case Cons(h, t) => f(h(), t().foldRight(z)(f))
+    case _ => z
+  }
+
+  def existsViaFoldRight(p: A => Boolean): Boolean = foldRight(false)((a,b) => p(a) || b)
+
+  def forAll(p: A => Boolean): Boolean = foldRight(true)((a,b) => p(a) && b)
+
+  def takeWhileViaFoldRight(p: A => Boolean): Stream[A] = foldRight(empty[A])((a,b) => if (p(a)) cons(a,b) else empty)
+
+  def headOptionViaFoldRight: Option[A] = foldRight(None: Option[A])((a,b) => Some(a))
+
+  def map[B](p: A => B): Stream[B] = foldRight(empty[B])((a,b) => cons(p(a),b))
+
+  def filter(p: A => Boolean): Stream[A] = foldRight(empty[A])((a,b) => if (p(a)) cons(a,b) else b)
+
+  def append[B>:A](s: => Stream[B]): Stream[B] = foldRight(s)((a,b) => cons(a,b))
+
+  def flatMap[B](p: A => Stream[B]): Stream[B] = foldRight(empty[B])((a,b) => p(a) append b)
+
+  def find(p: A => Boolean): Option[A] = filter(p).headOption
+
+  def constant[A](a: A): Stream[A] = cons(a, constant(a))
+  def infiniteOfIntegers(n: Int): Stream[Int] = cons(n, infiniteOfIntegers(n + 1))
+  def infiniteFibs(a: Int = 0, b: Int = 1): Stream[Int] = cons(a, infiniteFibs(b, a + b))
+
+  def unfold[A, S](z: S)(f: S => Option[(A,S)]): Stream[A] = ???
 }
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
