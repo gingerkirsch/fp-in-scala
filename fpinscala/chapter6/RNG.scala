2db1b41153c3e8949bd5d7ba1cf2072da4d48c14
Sasha Sankova <sandra.sankova@gmail.com>
2018-01-29 00:12:56 +0000

    RNG with state

---
+++ fpinscala/chapter6/RNG.scala
@@ -0,0 +1,36 @@
+trait RNG {
+  def nextInt: (Int, RNG)
+}
+
+case class SimpleRNG(seed: Long) extends RNG {
+  def nextInt: (Int, RNG) = {
+    val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+    val nextRNG = SimpleRNG(newSeed)
+    val n = (newSeed >>> 16).toInt
+    (n, nextRNG)
+  }
+
+  def randomPairSameValue(rng: RNG): (Int, Int) = {
+    val (i1, _) = rng.nextInt
+    val (i2, _) = rng.nextInt
+    (i1, i2)
+  }
+
+  def randomPair(rng: RNG): ((Int, Int), RNG) = {
+    val (i1, rng2) = rng.nextInt
+    val (i2, rng3) = rng2.nextInt
+    ((i1, i2), rng3)
+  }
+
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val (i, r) = rng.nextInt
+    if (i > 0 || i <= Int.MaxValue) (i, rng) else nonNegativeInt(rng)
+  }
+
+  def nonNegativeIntCorrect(rng: RNG): (Int, RNG) = {
+    val (i, r) = rng.nextInt
+    (if (i < 0) -(i + 1) else i, r)
+  }
+
+  def double(rng: RNG): (Double, RNG) = ???
+}
