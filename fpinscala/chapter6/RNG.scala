51ac151a78266db5aafdfccf1155add0e1c8eebc
Sasha Sankova <sandra.sankova@gmail.com>
2018-01-30 23:26:14 +0000

    More randoms

--- fpinscala/chapter6/RNG.scala
+++ fpinscala/chapter6/RNG.scala
@@ -1,3 +1,5 @@
+import scala.annotation.tailrec
+
 trait RNG {
   def nextInt: (Int, RNG)
 }
@@ -32,5 +34,38 @@ case class SimpleRNG(seed: Long) extends RNG {
     (if (i < 0) -(i + 1) else i, r)
   }
 
-  def double(rng: RNG): (Double, RNG) = ???
+  def double(rng: RNG): (Double, RNG) = {
+    val (i, r) = rng.nextInt
+    ((i / (Int.MaxValue.toDouble - 1)), r)
+  }
+
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val (i, r) = rng.nextInt
+    val (d, r2) = double(r)
+    ((i, d), r2)
+  }
+
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val (d, r) = double(rng)
+    val (i, r2) = r.nextInt
+    ((d, i), r2)
+  }
+
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val (d1, r1) = double(rng)
+    val (d2, r2) = double(r1)
+    val (d3, r3) = double(r2)
+    ((d1, d2, d3), r3)
+  }
+
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    @tailrec
+    def loop(count: Int = 0, r: RNG = rng, buffer: List[Int] = Nil): (List[Int], RNG)  = count match {
+      case 0 => (buffer, rng)
+      case _ =>
+        val (i, r2) = rng.nextInt
+        loop(count - 1, r2, i :: buffer)
+    }
+    loop(count)
+  }
 }
